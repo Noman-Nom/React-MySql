0fc8eaab034259324878acdc71e3ed2052b3e9b5
Noman <noman5456318@gmail.com>
2023-10-27 01:44:18 +0500

    addition working

--- src/CreateStudent.jsx
+++ src/CreateStudent.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import './CreateStudent.css'
 const CreateStudent = () => {
   return (
-    <div>CreateStudent</div>
+    <div>
+        <h1>Add New Students</h1>
+        <div className="form">
+            <div className='form__input'>
+                <label htmlFor="name">Name</label>
+                <input type="text" placeholder='enter name' id='name' onChange={()=>{}} />
+            </div>
+            <div className='form__input'>
+                <label htmlFor="name">Email</label>
+                <input type="text" placeholder='enter email' id='email' onChange={()=>{}} />
+            </div>
+        </div>
+    </div>
   )
 }
 
